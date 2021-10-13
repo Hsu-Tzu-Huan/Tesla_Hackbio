1ef51b54da432ad2b4db95875761b06cfb9c51df
Hsu-Tzu-Huan <beby99887766@gmail.com>
2021-10-13 21:24:02 +0800

    Add new file TYChef.php

---
+++ TYChef.php
@@ -0,0 +1,11 @@
+<?php
+$name = "Tzu Huan Hsu";
+$email = "hsutzuhuan88@gmail.com";
+$username = "@TYChef";
+$biostack = "tesla";
+
+echo "My name is $name.\n";
+echo "My email is $email.\n";
+echo "My slack username is $username.\n";
+echo "My biostack is $biostack.\n";
+?>
