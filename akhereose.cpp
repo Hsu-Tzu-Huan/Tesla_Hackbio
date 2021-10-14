7084b21c6f2b428efbdaac19cf07d7310b0b9e3b
Akhereose <92271512+Akhereose@users.noreply.github.com>
2021-10-15 00:07:59 +0100

    Add files via upload

---
+++ akhereose.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	string fname;
+	string lname;
+	string email;
+	string uname;
+	string biostack;
+	
+	cout<<"Enter First Name: ";
+	cin>>fname;
+	
+	cout<<"Enter Last Name: ";
+	cin>>lname;
+	
+	cout<<"Enter Email: ";
+	cin>>email;
+	
+	cout<<"Enter Username: ";
+	cin>>uname;
+	
+	cout<<"Enter Biostack: ";
+	cin>>biostack;
+	
+	cout<<"first Name: "<<fname<<endl;
+	cout<<"last Name: "<<lname<<endl;
+	cout<<"email: "<<email<<endl;
+	cout<<"username: "<<uname<<endl;
+	cout<<"biostack: "<<biostack<<endl;
+
+	return 0;
+}
