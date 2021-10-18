e6de160644698ff9db57262b39b4640d5e27ac21
pandathedev <winnyp48@gmail.com>
2021-10-18 10:45:42 +0300

    changes

---
+++ winnie.js
@@ -0,0 +1,40 @@
+
+
+
+    var username = 'winfred muteti';
+    var email = 'winnyp48@gmail.com';
+    var slackUsername = '@winnypius';
+    var biostack = 'genomics';
+  
+
+
+       console.log(username,',', email, ',', slackUsername,',', biostack,',', );
+
+  
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</html>
